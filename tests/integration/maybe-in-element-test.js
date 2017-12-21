88ef091fc2d1cb8dab330b1495f1b06487ace9ea
cibernox <miguel.camba@gmail.com>
2017-12-21 19:28:26 +0100

    TDD: Tests are ready

---
+++ tests/integration/maybe-in-element-test.js
@@ -0,0 +1,64 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | maybe-in-element', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('When the second argument is not given it renders the content in the destination element', async function(assert) {
+    await render(hbs`
+      <div id="test-destination-element"></div>
+      {{#if ready}}
+        {{#maybe-in-element destinationElement}}Some text{{/maybe-in-element}}
+      {{/if}}
+    `);
+    this.set('destinationElement', this.element.querySelector('#test-destination-element'));
+    this.set('ready', true);
+    assert.dom('#test-destination-element').hasText('Some text', 'The content has been rendered in the destination element');
+  });
+
+  test('When the second argument is falsy it renders the content in the destination element', async function(assert) {
+    this.renderInPlace = false;
+    await render(hbs`
+      <div id="test-destination-element"></div>
+      {{#if ready}}
+        {{#maybe-in-element destinationElement renderInPlace}}Some text{{/maybe-in-element}}
+      {{/if}}
+    `);
+    this.set('destinationElement', this.element.querySelector('#test-destination-element'));
+    this.set('ready', true);
+    assert.dom('#test-destination-element').hasText('Some text', 'The content has been rendered in the destination element');
+  });
+
+  test('When the second argument is true it renders the content in the destination element', async function(assert) {
+    this.renderInPlace = true;
+    await render(hbs`
+      <div id="test-destination-element"></div>
+      {{#if ready}}
+        <div id="original-placement">{{#maybe-in-element destinationElement renderInPlace}}Some text{{/maybe-in-element}}</div>
+      {{/if}}
+    `);
+    this.set('destinationElement', this.element.querySelector('#test-destination-element'));
+    this.set('ready', true);
+    assert.dom('#test-destination-element').doesNotHaveText('Some text', 'The content wasn\'t rendered in the destination element');
+    assert.dom('#original-placement').hasText('Some text', 'The content has been rendered in the original location');
+  });
+
+  test('The second argument is bound so the content can move from the destination element to the original source at will', async function (assert) {
+    this.renderInPlace = true;
+    await render(hbs`
+      <div id="test-destination-element"></div>
+      {{#if ready}}
+        <div id="original-placement">{{#maybe-in-element destinationElement renderInPlace}}Some text{{/maybe-in-element}}</div>
+      {{/if}}
+    `);
+    this.set('destinationElement', this.element.querySelector('#test-destination-element'));
+    this.set('ready', true);
+    assert.dom('#test-destination-element').doesNotHaveText('Some text', 'The content wasn\'t rendered in the destination element');
+    assert.dom('#original-placement').hasText('Some text', 'The content has been rendered in the original location');
+    this.set('renderInPlace', false);
+    assert.dom('#test-destination-element').hasText('Some text', 'The content has been rendered in the destination element');
+    assert.dom('#original-placement').doesNotHaveText('Some text', 'The content wasn\'t rendered in the original location');
+  });
+});
