2aa83622eebab1d9a911c2b208d974c032bdc408
cibernox <miguel.camba@gmail.com>
2017-12-24 00:24:13 +0100

    Allow dinamic destination

--- tests/dummy/app/components/component-using-maybe-in-element.js
+++ tests/dummy/app/components/component-using-maybe-in-element.js
@@ -1,15 +1,25 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   renderInPlace: false,
+  id: '#in-element-destination',
 
-  init() {
-    this._super(...arguments);
-    this.destinationElement = document.querySelector('#in-element-destination');
-  },
+  destinationElement: computed('id', function() {
+    return document.querySelector(this.get('id'));
+  }),
+
   actions: {
     toggleRenderInPlace() {
       this.set('renderInPlace', !this.get('renderInPlace'));
+    },
+
+    changeDestination() {
+      if (this.get('id') === '#in-element-destination') {
+        this.set('id', '#in-element-destination2');
+      } else {
+        this.set('id', '#in-element-destination');
+      }
     }
   }
 });
