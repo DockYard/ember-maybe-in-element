322f1d89e5945d86211b806ceb8b338d0c622da6
Miguel Camba <miguel.camba@gmail.com>
2020-07-05 00:29:01 +0200

    This seems to work locally

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ module.exports = {
   name: require('./package').name,
 
   setupPreprocessorRegistry(type, registry) {
-    let plugins = registry.load('htmlbars-ast-plugin');
-    let inElementPlugin = plugins.find((plugin) => plugin.name === 'ember-in-element-polyfill');
     let maybePlugin = {
       name: 'ember-maybe-in-element-transform',
       plugin: EmberMaybeInElementAstTransform,
@@ -16,17 +14,6 @@ module.exports = {
       }
     };
 
-    // Yes, this a bit ugly, but it seems for some reason AST plugins are applied in a different order (reversed?) than
-    // the order they are added to the registry.
-    // With this dirty hack we make sure there is always a "polyfill" transform running after the "maybe" transform, to make
-    // sure the AST returned from "maybe" containing `{{in-element}}` gets further transformed by the polyfill!
-    if (inElementPlugin) {
-      registry.remove('htmlbars-ast-plugin', inElementPlugin);
-      registry.add('htmlbars-ast-plugin', inElementPlugin);
-      registry.add('htmlbars-ast-plugin', maybePlugin);
-      registry.add('htmlbars-ast-plugin', inElementPlugin);
-    } else {
-      registry.add('htmlbars-ast-plugin', maybePlugin);
-    }
+    registry.add('htmlbars-ast-plugin', maybePlugin);
   },
 };
