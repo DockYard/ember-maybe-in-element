be154f62e6555090803d23eac7a4fa0f328cd5d4
Chris Thoburn <runspired@gmail.com>
2022-08-04 00:32:14 -0700

    fix and bump deps

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ module.exports = {
   name: require('./package').name,
 
   setupPreprocessorRegistry(type, registry) {
+    if (type !== 'parent') {
+			return;
+		}
     let checker = new VersionChecker(this.project);
     let dep = checker.for('ember-source');
-    let maybePlugin = {
+    let version = dep.gte('3.17.0');
+		const plugin = this._buildPlugin({ version });
+    plugin.parallelBabel = {
+			requireFile: __filename,
+			buildUsing: '_buildPlugin',
+			params: options,
+		};
+
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+	_buildPlugin(options) {
+    return {
       name: 'ember-maybe-in-element-transform',
-      plugin: buildAstTransform(dep.gte('3.17.0')),
+      plugin: buildAstTransform(options.version),
       baseDir() {
         return __dirname;
       },
     };
-    registry.add('htmlbars-ast-plugin', maybePlugin);
-  },
+	},
 };
